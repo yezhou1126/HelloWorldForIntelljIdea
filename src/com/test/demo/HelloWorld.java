b149911876d3a913cee828a67c9cbfda36de3edc
JohnnyYoung <yezhouyang2008@qq.com>
2017-02-06 12:13:52 +0800

    Initial commit

---
+++ src/com/test/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.test.demo;
+
+/**
+ * Created by Johnn on 2017/2/6.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
+}
